f9c617215551e14384838915428227e1f6dc5f15
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-01-28 13:17:24 +0100

    feat: Add logging output into the class "Repeater"

--- univention/directory_importer/util.py
+++ univention/directory_importer/util.py
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+import logging
 import time
 from typing import Callable
+
+log = logging.getLogger(__name__)
 
 
 class Repeater:
@@ -17,8 +20,10 @@ class Repeater:
         if delay is None:
             delay = Repeater.DEFAULT_DELAY
         self.delay = delay
+        log.debug("Repeating delay set to %i", self.delay)
 
     def call(self, *args, **kwargs):
         while True:
             self.target(*args, **kwargs)
+            log.info("Sleeping %i seconds", self.delay)
             time.sleep(self.delay)
