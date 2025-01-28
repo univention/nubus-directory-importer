659a84e9de588d5beabe0a35fce36885fea9bc81
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-01-28 11:35:09 +0100

    test: Move mocking of connector and config into fixtures

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,40 +13,43 @@ from univention.directory_importer.__main__ import app
 runner = CliRunner()
 
 
+@pytest.fixture(autouse=True)
+def mock_connector(mocker):
+    """Replace the Connector object with a Mock."""
+    return mocker.patch.object(__main__, "Connector")
+
+
+@pytest.fixture
+def mock_connector_config(mocker):
+    return mocker.patch.object(__main__, "ConnectorConfig")
+
+
 @pytest.fixture
 def stub_config(tmp_path):
-    """Creates a stub configuration file"""
+    """Creates a stub configuration file."""
     stub_config = tmp_path / "config.yaml"
     stub_config.write_text("")
     return stub_config
 
 
-def test_pass_config_filename_as_cli_option(mocker, stub_config):
-    connector_config_mock = mocker.patch.object(__main__, "ConnectorConfig")
-    mocker.patch.object(__main__, "Connector")
-
+def test_pass_config_filename_as_cli_option(mock_connector_config, stub_config):
     result = runner.invoke(app, [str(stub_config)])
-
     assert result.exit_code == 0
-    connector_config_mock.assert_called_once_with(stub_config)
+    mock_connector_config.assert_called_once_with(stub_config)
 
 
-def test_reads_config_filename_from_environment(mocker, stub_config):
-    connector_config_mock = mocker.patch.object(__main__, "ConnectorConfig")
-    mocker.patch.object(__main__, "Connector")
-
+def test_reads_config_filename_from_environment(mock_connector_config, stub_config):
     result = runner.invoke(app, [], env={"AD2UCS_CFG": str(stub_config)})
-
     assert result.exit_code == 0
-    connector_config_mock.assert_called_once_with(stub_config)
+    mock_connector_config.assert_called_once_with(stub_config)
 
 
-def test_fails_if_config_path_is_a_directory(mocker, tmp_path):
+def test_fails_if_config_path_is_a_directory(tmp_path):
     result = runner.invoke(app, str(tmp_path))
     assert result.exit_code != 0
 
 
-def test_fails_if_config_file_does_not_exist(mocker, tmp_path):
+def test_fails_if_config_file_does_not_exist(tmp_path):
     missing_file = tmp_path / "does-not-exist.yaml"
     result = runner.invoke(app, str(missing_file))
     assert result.exit_code != 0
